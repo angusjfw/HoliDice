030120c72d7566af8a6e7077faae0f4ef5112453
hibreez <4ngus.whitehead@gmail.com>
2016-02-02 17:23:55 +0000

    Adds feature test for searching and getting flight results.

--- test/e2e/holiDiceFeatures.js
+++ test/e2e/holiDiceFeatures.js
@@ -1,7 +1,24 @@
 describe('HoliDice', function() {
-  it('has a title', function() {
-    browser.get('http://localhost:8080');
 
+  beforeAll(function() {
+    browser.get('http://localhost:8080');
+    startLocation = element(by.model('HDCtrl.startLocation'));
+    searchButton = element(by.css('[ng-click="HDCtrl.doSearch()"]'));
+    flightResults = element.all(by.repeater('flight in HDCtrl.fightResults'));
+    expectedResult = {
+      "origin": "LHR",
+      "destination": "BOS",
+      "date": "2016-02-04"
+    };
+  });
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('HoliDice');
+  });
+
+  it('suggests flights for the right locations and date', function() {
+    startLocation.sendKeys('LHR');
+    searchButton.click();
+    expect(flightResults.first().getText()).toEqual();
   });
 });

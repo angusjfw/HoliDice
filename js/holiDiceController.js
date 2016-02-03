3618134c5ff606dc7604722546bc5ea78f0157c6
gareth4192 <gareth-123@live.com>
2016-02-03 10:36:20 +0000

    api returning data set, not yet being displayed

--- js/holiDiceController.js
+++ js/holiDiceController.js
@@ -2,11 +2,13 @@ holiDice.controller('HoliDiceController', ['FlightSearch', function(FlightSearch
   var self = this;
 
   self.startingLocation = '';
+  self.holidayLocation = 'LAX';
 
   self.doSearch = function (){
-    FlightSearch.query()
+    FlightSearch.query(self.startingLocation, self.holidayLocation)
       .then(function(response) {
         self.flightResults = response;
+        console.log(response);
       });
   };
 

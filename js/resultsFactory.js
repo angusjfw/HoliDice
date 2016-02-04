e8c3241a610849462097aee8273c78ec1a3baf39
hibreez <4ngus.whitehead@gmail.com>
2016-02-04 11:04:36 +0000

    Shows full airport name and formats date and price.

--- js/resultsFactory.js
+++ js/resultsFactory.js
@@ -1,37 +1,56 @@
 holiDice.factory('ResultsFactory', [function() {
   var factory = {};
 
-  factory.outboundName = function(flightResults){
-    return flightResults.tripOption[0].slice[0].segment[0].leg[0].origin
-  };
-
-  factory.inboundName = function(flightResults){
-    return flightResults.tripOption[0].slice[0].segment[0].leg[0].destination
-  };
-
-  factory.price = function(flightResults){
-    return flightResults.tripOption[0].saleTotal
-  };
-
-  factory.flightNumber = function(flightResults){
-    return flightResults.tripOption[0].slice[0].segment[0].flight.number
-  };
-
-  factory.departureTime = function(flightResults){
-    return flightResults.tripOption[0].slice[0].segment[0].leg[0].departureTime
-  };
-
-  factory.carrierName = function(flightResults){
-    return flightResults.data.carrier[0].name
-  };
-
   factory.validate = function(flightResults) {
-    if(typeof flightResults.tripOption == "undefined" ){
-      return false
+    if(typeof flightResults.tripOption == "undefined" ) {
+      return false;
     }
-    else {return true}
+    else { return true; }
   };
 
+  factory.outboundName = function(flightResults) {
+    var iata = flightResults.tripOption[0].slice[0].segment[0].leg[0].origin;
+    return factory.nameFromIata(factory.airports(flightResults), iata);
+  };
 
-return factory;
+  factory.inboundName = function(flightResults) {
+    var iata = flightResults.tripOption[0].slice[0].segment[0].leg[0].destination;
+    return factory.nameFromIata(factory.airports(flightResults), iata);
+  };
+
+  factory.price = function(flightResults) {
+    var price = flightResults.tripOption[0].saleTotal;
+    var currency = price.substring(0, 3) === "GBP" ? "£":price.substring(0, 3);
+    return currency + price.substring(3, price.length);
+  };
+
+  factory.flightNumber = function(flightResults) {
+    return flightResults.tripOption[0].slice[0].segment[0].flight.number;
+  };
+
+  factory.departureTime = function(flightResults) {
+    var date = flightResults.tripOption[0].slice[0].segment[0].leg[0].departureTime;
+    return new Date(date).toUTCString();
+  };
+
+  factory.carrierName = function(flightResults) {
+    return flightResults.data.carrier[0].name;
+  };
+
+  factory.airports = function(flightResults) {
+    return flightResults.data.airport;
+  };
+
+  factory.nameFromIata = function(airports, iata) {
+    var match;
+    airports.forEach(function(airport) {
+      if (airport.code === iata) {
+        match = airport.name;
+      }
+    });
+    if (match) { return match; }
+    else { return iata; }
+  };
+
+  return factory;
 }]);

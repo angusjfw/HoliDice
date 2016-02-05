1ca7e459b0428a2d88ca171e7fb0d49416fd3550
hibreez <4ngus.whitehead@gmail.com>
2016-02-05 00:48:10 +0000

    Updates randomAirport factory to use new aiportSelection data and deletes old iatas array.

--- js/randomAirportFactory.js
+++ js/randomAirportFactory.js
@@ -1,9 +1,9 @@
 holiDice.factory('RandomAirport', [function() {
   return {
     query: function() {
-      return airportIATAs[
-        Math.floor(Math.random() * airportIATAs.length)
-      ];
+      return allAirports[
+        Math.floor(Math.random() * allAirports.length)
+      ].iata;
     }
   };
 }]);

150393cc75368c5ba9a6bb25575402442b41c928
hibreez <4ngus.whitehead@gmail.com>
2016-02-05 12:01:19 +0000

    Refactor controller, moving looping and calling ranomiser responsibilities to FlightSearch factory.

--- js/flightSearchFactory.js
+++ js/flightSearchFactory.js
@@ -1,26 +1,50 @@
 holiDice.factory('FlightSearch', ['$http', function($http) {
+  var factory = {};
+
   var queryUrl = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=';
   var apiKey = googleAPIKey;
+  var DEFAULTLOCATION = "BCN";
+  var MAXREQUESTS = 20;
 
-  return {
-    query: function(startLocation, holidayLocation, depDate, returnDate) {
-      return $http.post(
+  factory.requests = 0;
+
+  factory.query = function(startLocation, holidayLocation, depDate, returnDate, cb) {
+    factory.callAPI(startLocation, holidayLocation(), depDate, returnDate)
+      .then(function(response) {
+        factory.results = response.data.trips;
+
+        if (!factory.validate(factory.results)) {
+          if (factory.requests < MAXREQUESTS) {
+            factory.query(startLocation, holidayLocation, depDate, returnDate, cb);
+            factory.requests += 1;
+          } else {
+            factory.query(startLocation, DEFAULTLOCATION, depDate, returnDate, cb);
+          }
+        } else {
+          factory.requests = 0;
+          cb(factory.results);
+        }
+      });
+  };
+
+  factory.callAPI = function(startLocation, holidayLocation, depDate, returnDate) {
+    return $http.post(
         queryUrl + apiKey,
         {
           "request": {
             "slice": [
-              {
-                "origin": startLocation,
-                "destination": holidayLocation,
-                "date": depDate,
-                // "maxConnectionDuration": 0
-              },
-              {
-                "origin": holidayLocation,
-                "destination": startLocation,
-                "date": returnDate,
-                // "maxConnectionDuration": 0
-              }
+            {
+              "origin": startLocation,
+              "destination": holidayLocation,
+              "date": depDate,
+              // "maxConnectionDuration": 0
+            },
+            {
+              "origin": holidayLocation,
+              "destination": startLocation,
+              "date": returnDate,
+              // "maxConnectionDuration": 0
+            }
             ],
             "passengers": {
               "adultCount": 1,
@@ -34,7 +58,12 @@ holiDice.factory('FlightSearch', ['$http', function($http) {
             "refundable": false
           }
         }
-      );
-    }
+    );
   };
+
+  factory.validate = function(results) {
+    return (results.tripOption !== undefined);
+  };
+
+  return factory;
 }]);
--- js/holiDiceController.js
+++ js/holiDiceController.js
@@ -3,49 +3,42 @@ holiDice.controller('HoliDiceController',
     function(RandomAirport, FlightSearch, ResultsFactory) {
 
   var self = this;
-  var MAXREQUESTS = 20;
 
   self.startLocation = '';
-  self.holidayLocation = '';
   self.depDate = '';
   self.returnDate = '';
   self.validate = false;
   self.loading = false;
-  self.requests = 0;
 
 
   self.doSearch = function (){
     self.validate = false;
     self.loading = true;
 
-    self.holidayLocation = RandomAirport.query();
+    FlightSearch.query(
+        self.startLocation,
+        RandomAirport.query,
+        self.depDate,
+        self.returnDate,
 
-    FlightSearch.query(self.startLocation, self.holidayLocation,
-                       self.depDate, self.returnDate)
-      .then(function(response) {
-        self.flightResults = response.data.trips;
-
-        if (!ResultsFactory.validate(self.flightResults) &&
-            self.requests < MAXREQUESTS) {
-          self.requests += 1;
-          self.doSearch();
-        } else if (self.requests === MAXREQUESTS) {
-          self.holidayLocation = "BCN";
-          self.doSearch();
-        } else {
+        function(results) {
           self.loading = false;
           self.validate = true;
-          self.requests = 0;
-
-          self.outboundName = ResultsFactory.outboundName(self.flightResults);
-          self.inboundName = ResultsFactory.inboundName(self.flightResults);
-          self.price = ResultsFactory.price(self.flightResults);
-          self.flightNumber = ResultsFactory.flightNumber(self.flightResults);
-          self.departureTime = ResultsFactory.departureTime(self.flightResults);
-          self.carrierName = ResultsFactory.carrierName(self.flightResults);
-          self.buyURL = ResultsFactory.buyURL(self.startLocation,
-                        self.holidayLocation, self.depDate, self.returnDate);
+          self.flightResults = results;
+          self.showResults();
         }
-      });
+    );
+  };
+
+  self.showResults = function() {
+    self.outboundName = ResultsFactory.outboundName(self.flightResults);
+    self.inboundName = ResultsFactory.inboundName(self.flightResults);
+    self.price = ResultsFactory.price(self.flightResults);
+    self.flightNumber = ResultsFactory.flightNumber(self.flightResults);
+    self.departureTime = ResultsFactory.departureTime(self.flightResults);
+    self.carrierName = ResultsFactory.carrierName(self.flightResults);
+    self.holidayLocation = ResultsFactory.inboundIata(self.flightResults);
+    self.buyURL = ResultsFactory.buyURL(self.startLocation,
+        self.holidayLocation, self.depDate, self.returnDate);
   };
 }]);
